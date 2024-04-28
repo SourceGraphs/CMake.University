c0ea766580da6e2c7def1def7b60eda176adea71
DarkStar <darkstar@unisocial.net>
2024-04-28 14:17:04 -0500

    template

---
+++ template:/by: LearnQtGuide/simplest/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fmt/format.h>
+
+int main(int, char**){
+    std::cout << "Hello, from try!\n";
+
+    fmt::println("The sky is blue");
+}
+
+//from: [2024 C++ and CMake Setup in Visual Studio Code: A Step-by-Step Guide](https://youtu.be/4U-lnfxY2U0)
