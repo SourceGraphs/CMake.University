6f956d499ef60e2b045ee844d144ea0cd5a09859
DarkStar <darkstar@unisocial.net>
2024-04-28 15:47:13 -0500

    1st

--- template:/by: LearnQtGuide/simplest/src/main.cpp
+++ template:/by: LearnQtGuide/simplest/src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <fmt/format.h>
+#include "person.h"
 
 int main(int, char**){
     std::cout << "Hello, from try!\n";
 
-    fmt::println("The sky is blue");
+    fmt::println("The sky is blue 42");
+
+    Person p;
+    p.eat();
 }
 
 //from: [2024 C++ and CMake Setup in Visual Studio Code: A Step-by-Step Guide](https://youtu.be/4U-lnfxY2U0)
--- template:/by: LearnQtGuide/simplest/src/readme.md
+++ template:/by: LearnQtGuide/simplest/src/readme.md
@@ -1,1 +1,4 @@
 from: [2024 C++ and CMake Setup in Visual Studio Code: A Step-by-Step Guide](https://youtu.be/4U-lnfxY2U0)
+
+source:
+- https://github.com/rutura/vscodecpp/tree/main/1.TasksWindows
